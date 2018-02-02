6e1fe380b8e4e60f355a330724312c8a0e17410f
Karl Yerkes <karl.yerkes@gmail.com>
2018-02-02 11:02:09 -0800

    MAT 201B Agent Systems (Starter Code)

--- color_spaces/color_spaces.cpp
+++ color_spaces/color_spaces.cpp
@@ -1,109 +1,127 @@
-#include <cassert>
-#include <iostream>
 #include "allocore/io/al_App.hpp"
 using namespace al;
 using namespace std;
 
-class MyApp : public App {
- public:
+struct MyApp : App {
   Image image;
-  Texture texture;
+  Mesh plane, cube, cylinder, fun, current, target, last;
 
   MyApp() {
-    // Load a .jpg file
-    //
-    const char* filename = "mat201b/color_spaces/double-rainbow.jpg";
-
-    if (image.load(filename)) {
-      printf("Read image from %s\n", filename);
-    } else {
-      printf("Failed to read image from %s!  Quitting.\n", filename);
-      exit(-1);
+    const char* filename = "mat201b/color_spaces/photoshop.png";
+    // const char* filename = "mat201b/color_spaces/Tv-Snow.jpg";
+    // const char* filename = "mat201b/color_spaces/double-rainbow.jpg";
+    if (!image.load(filename)) {
+      cerr << "ERROR" << endl;
+      exit(1);
     }
-
-    // Here we copy the pixels from the image to the texture
-    texture.allocate(image.array());
-
-    // Don't bother trying to print the image or the image's array directly
-    // using C++ syntax.  This won't work:
-    //
-    // cout << "Image " << image << endl;
-    // cout << "   Array: " << image.array() << endl;
-
-    // Make a reference to our image's array so we can just say "array" instead
-    // of "image.array()":
-    //
     Array& array(image.array());
 
-    // The "components" of the array are like "planes" in Jitter: the number of
-    // data elements in each cell.  In our case three components would
-    // represent R, G, B.
-    //
     cout << "array has " << (int)array.components() << " components" << endl;
-
-    // Each of these data elements is represented by the same numeric type:
-    //
     cout << "Array's type (as enum) is " << array.type() << endl;
-
-    // But that type is represented as an enum (see al_Array.h), so if you want
-    // to read it use this function:
-    //
     printf("Array's type (human readable) is %s\n",
            allo_type_name(array.type()));
-
-    // The array itself also has a print method:
-    //
     cout << "Array.print: " << endl << "   ";
     array.print();
-
-    // Code below assumes this type is 8-bit unsigned integer, so this line
-    // guarantees that's the case, or else crashes the program if not:
-    //
     assert(array.type() == AlloUInt8Ty);
 
-    // AlloCore's image class provides a type for an RGBA pixel, which is of
-    // course templated on the numeric type used to represent each value in the
-    // pixel.  Since templating happens at compile time we can't just ask the
-    // array at runtime what type to put in here (hence the "assert" above):
     //
+
     Image::RGBAPix<uint8_t> pixel;
 
-    // Loop through all the pixels.  Note that the columns go from left to
-    // right and the rows go from bottom to top.  (So the "row" and "column"
-    // are like X and Y coordinates on the Cartesian plane, with the entire
-    // image living in the quadrant with positive X and positive Y --- in other
-    // words the origin is in the lower left of the image.)
-    //
-    cout << "Display ALL the pixels !!! " << endl;
+    int W = array.width();
+    int H = array.height();
+    float aspectRatio = W / (float)H;
+    cout << "aspectRatio: " << aspectRatio << endl;
 
-    for (size_t row = 0; row < array.height(); ++row) {
-      for (size_t col = 0; col < array.width(); ++col) {
-        // read the pixel at (row, col) and print
+    for (size_t row = 0; row < H; ++row) {
+      for (size_t col = 0; col < W; ++col) {
+        array.read(&pixel.r, col, row);  // XXX &pixel.r versus &pixel
+        Color color(pixel.r / 256.0f, pixel.g / 256.0f, pixel.b / 256.0f, 0.6);
+
+        current.color(color);
+
         //
-        // array.read(&pixel, row, col);
-        array.read(&pixel, col, row);
-        // you can ask pixel.g? (0, 255)
+        current.vertex(1.0f * col / W, aspectRatio * row / H, 0);
+        plane.vertex(1.0f * col / W, aspectRatio * row / H, 0);
+        last.vertex(1.0f * col / W, aspectRatio * row / H, 0);
+        target.vertex(1.0f * col / W, aspectRatio * row / H, 0);
+
+        //
+        cube.vertex(color.r, color.g, color.b);
+
+        {
+          HSV c(color);
+          cylinder.vertex(c.s * sin(2 * M_PI * c.h), c.v,
+                          c.s * cos(2 * M_PI * c.h));
+        }
+
+        {
+          Lab c(color);
+          fun.vertex(c.l / 100.0f, c.a / 100.0f, c.b / 100.0f);
+          // printf("%f %f %f\n", c.l, c.a, c.b);
+        }
       }
     }
+
+    nav().pos(0, 0, 7);
+  }
+
+  double t = 1000;  // an animation parameter and mode flag
+  double angle = 0;
+  void onAnimate(double dt) {
+    if (t < 1) {
+      for (int v = 0; v < current.vertices().size(); v++)
+        current.vertices()[v] =
+            target.vertices()[v] * t + last.vertices()[v] * (1 - t);
+      // current.vertices()[v].lerp(target.vertices()[v], 0.5);
+    } else if (t > 100)
+      ;  // do nothing
+    else {
+      for (int v = 0; v < current.vertices().size(); v++)
+        current.vertices()[v] = target.vertices()[v];
+      t += 1000;
+    }
+
+    t += dt;
+    angle += dt;
   }
 
   void onDraw(Graphics& g) {
-    g.pushMatrix();
-
-    // Push the texture/quad back 5 units (away from the camera)
     //
-    g.translate(0, 0, -5);
+    g.rotate(angle * 33, Vec3f(0, 1, 0));
+    g.scale(1.7);
+    g.draw(current);
+  }
 
-    // See void Texture::quad(...) in the Doxygen
-    //
-    texture.quad(g);
-
-    g.popMatrix();
+  void onKeyDown(const ViewpointWindow&, const Keyboard& k) {
+    switch (k.key()) {
+      default:
+      case '1':
+        last = target;
+        target = plane;
+        t = 0;
+        break;
+      case '2':
+        last = target;
+        target = cube;
+        t = 0;
+        break;
+      case '3':
+        last = target;
+        target = cylinder;
+        t = 0;
+        break;
+      case '4':
+        last = target;
+        target = fun;
+        t = 0;
+        break;
+    }
   }
 };
 
 int main() {
   MyApp app;
-  app.initWindow(Window::Dim(600, 400), "imageTexture");
+  app.initWindow();
   app.start();
 }
